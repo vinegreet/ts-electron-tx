7fdc31757bcf81c7e3dd867b10f057210049ebc6
Roman Zots <vinegreet@gmail.com>
2021-09-27 18:14:45 +0300

    Wrap App into DataState

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import DataState from './context/DataState';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DataState>
+      <App />
+    </DataState>
   </React.StrictMode>,
   document.getElementById('root')
 );
