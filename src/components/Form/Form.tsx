5b567dee20e6b1882a2bf2dfaca5f17a3547c8dc
Roman Zots <vinegreet@gmail.com>
2021-09-27 23:24:37 +0300

    Update Form; add styles for it

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -4,41 +4,48 @@ import { requestStatus } from '../../context/DataState';
 import { defaultTxParams, getTxRequestUrl } from '../../models/getTxRequestUrl';
 import { IApiTxData } from '../../models/IData';
 
-const Form = () => {
-  const [ address, setAddress ] = useState('');
+import './form.css';
+
+const Form: React.FC = () => {
+  const [ address, setAddress ] = useState('0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a');
   const [ apiKey, setApiKey ] = useState('');
   const { getTransactions } = useContext(DataContext);
+  const isButtonDisabled = address === '' && apiKey === '';
 
   const handleButtonClick = (): void => {
     (async () => {
-      if (address !== '' && apiKey !== '') {
-        try {
-          const response = await fetch(getTxRequestUrl({ ...defaultTxParams, address, apiKey }));
-          const data: IApiTxData = await response.json();
-          getTransactions(data.status, data);
-        } catch (err) {
-          getTransactions(requestStatus.failure);
-          console.error(err);
-        }
+      try {
+        const response = await fetch(getTxRequestUrl({ ...defaultTxParams, address, apiKey }));
+        const data: IApiTxData = await response.json();
+        getTransactions(data.status, data);
+      } catch (err) {
+        getTransactions(requestStatus.failure);
+        console.error(err);
       }
     })();
   }
 
   return (
     <div className="tx_form">
+      <label className="input-label">Ether address</label>
       <input
-        className="tx_address"
+        className="input tx_address"
         type="text"
+        placeholder="Ether address"
         onChange={(e) => setAddress(e.target.value)}
+        value={address}
       />
+      <label className="input-label">Your API key</label>
       <input
-        className="tx_api-key"
+        className="input tx_api-key"
         type="text"
+        placeholder="Your API key"
         onChange={(e) => setApiKey(e.target.value)}
       />
       <button
-        className="tx_get-transactions"
+        className="button tx_get-transactions"
         onClick={handleButtonClick}
+        disabled={isButtonDisabled}
       >
         Get transactions
       </button>
